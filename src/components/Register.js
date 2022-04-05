37ddfec13593adbdd841bd5a2d467a0e5584bc61
Dmitry <61393026+LaykDimon@users.noreply.github.com>
2022-04-05 15:57:46 +0300

    fixed registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,6 +11,7 @@ export default function Register() {
     
     const [submitted, setSubmitted] = useState(false);
     const [error, setError] = useState(false);
+    const [errorMessageText, setErrorMessageText] = useState(false);
 
     const handleUsername = (e) => {
         setUsername(e.target.value);
@@ -32,11 +33,43 @@ export default function Register() {
         setSubmitted(false);
     };
 
+
+    const emailValidation = () => {
+      const regex = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+      if(!email || regex.test(email) === false) {
+        setError(true);
+        setErrorMessageText('The email provided is wrong.');
+        return false;
+      }
+      return true;
+    };
+
+    const usernameValidation = () => {
+      if(!username || username.length > 255 || username.length < 3) {
+        setError(true);
+        setErrorMessageText('Username must be at least 3 characters and maximum 255 characters.');
+        return false;
+      }
+      return true;
+    }
+    
+    const passwordValidation = () => {
+      if(!password || !confirmPassword || password.length < 8 || confirmPassword.length < 8) {
+        setError(true);
+        setErrorMessageText('Password must be at least 8 characters.');
+        return false;
+      }
+      else if (password !== confirmPassword) {
+        setError(true);
+        setErrorMessageText('Passwords don\'t match.');
+        return false;
+      }
+      return true;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (username === '' || email === '' || password === '' || confirmPassword === '' || password !== confirmPassword) {
-          setError(true);
-        } else {
+        if (usernameValidation() && emailValidation() && passwordValidation()) {
           setSubmitted(true);
           setError(false);
           register();
@@ -50,7 +83,7 @@ export default function Register() {
             style={{
               display: submitted ? '' : 'none',
             }}>
-            <h1>User {username} successfully registered!!</h1>
+            <h1 className='successMsg'>User {username} successfully registered!!</h1>
           </div>
         );
     };
@@ -62,7 +95,7 @@ export default function Register() {
             style={{
               display: error ? '' : 'none',
             }}>
-            <h1>Please enter all the fields or check the passwords</h1>
+            <h1 className='errorMsg'>{errorMessageText}</h1>
           </div>
         );
     };
@@ -96,26 +129,26 @@ export default function Register() {
       <form method='post'>
           {/* Labels and inputs for form data */}
           <div className='txtField'>
-              <input onChange={handleUsername} value={username} type='text' required />
+              <input onChange={handleUsername} value={username} type='text' required id ='username'/>
               <span></span>
               <label>Username</label>
           </div>
           <div className='txtField'>
-              <input onChange={handleEmail} value={email} type='email' required />
+              <input onChange={handleEmail} value={email} type='email' required id='email' />
               <span></span>
               <label>Email</label>
           </div>
           <div className='txtField'>
-              <input onChange={handlePassword} value={password} type='password' required />
+              <input onChange={handlePassword} value={password} type='password' required id='password' />
               <span></span>
               <label>Password</label>
           </div>
           <div className='txtField'>
-              <input onChange={handleConfirmPassword} value={confirmPassword} type='password' required />
+              <input onChange={handleConfirmPassword} value={confirmPassword} type='password' required id='confirmPassword' />
               <span></span>
               <label>Confirm password</label>
           </div>
-          <input type='submit' value='Register' onClick={handleSubmit} />
+          <input type='submit' value='Register' id='submitBtn' onClick={handleSubmit} />
           <div className='loginLink'>
               Already signed up? <Link className='navLink' to="/login">Login</Link>
           </div>
